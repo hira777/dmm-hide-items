3298e51ec0d155d49d7f25db91cdd3e417009b90
soarflat <soarflat777@gmail.com>
2018-04-27 01:39:40 +0900

    feat: オプションUI更新

--- shells/chrome/src/options.js
+++ shells/chrome/src/options.js
@@ -1,11 +1,11 @@
-import * as chromeStorage from './storage';
+import * as store from './store';
 
 const optionIsHideItems = document.getElementById('hide-items');
 
-chromeStorage.init().then(() => {
-  optionIsHideItems.checked = chromeStorage.isExcludedVR();
+store.init().then(() => {
+  optionIsHideItems.checked = store.isHideItems();
   optionIsHideItems.onchange = e => {
-    chromeStorage.setIsHideItems(e.target.checked).then(() => {
+    store.setIsHideItems(e.target.checked).then(() => {
       console.log('optionIsHideItems');
     });
   };
